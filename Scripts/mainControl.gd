2a36691474b180f9c74629733b27ed61154144e2
Quentin DUPONT <quentindupont57@gmail.com>
2023-12-06 16:31:42 +0100

    scenes ameliores

--- Scripts/mainControl.gd
+++ Scripts/mainControl.gd
@@ -1,14 +1,10 @@
 extends Node
 
 @onready var mainMenu = preload("res://Scenes/MainMenuScene.tscn")
-@onready var mainMenuHud = preload("res://Scenes/MainMenuHud.tscn")
-@onready var gameplayHud = preload("res://Scenes/GameplayHud.tscn")
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Game.add_child(mainMenu.instantiate())
-	$HUD.add_child(mainMenuHud.instantiate())
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,8 +14,5 @@ func _process(delta):
 func loadNewScene(link):
 	for c in $Game.get_children():
 		c.queue_free()
-	for c in $HUD.get_children():
-		c.queue_free()
 	var newScene = load(link)
 	$Game.add_child(newScene.instantiate())
-	$HUD.add_child(gameplayHud.instantiate())
